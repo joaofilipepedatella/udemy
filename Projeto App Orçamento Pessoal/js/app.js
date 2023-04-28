1f111ade93bee5bd5dc1ed0a5433bd3fc8d1e056
João Filipe Pedatella Ferreira Marques <126195945+joaofilipepedatella@users.noreply.github.com>
2023-04-28 19:35:42 -0300

    finalização projeto

--- Projeto App Orçamento Pessoal/js/app.js
+++ Projeto App Orçamento Pessoal/js/app.js
@@ -60,10 +60,46 @@ class Bd {
                 continue
             }
 
+            despesa.id = i
             despesas.push(despesa)
         }
 
         return despesas
+    }
+
+    pesquisar(despesa){
+        let despesasFiltradas = Array()
+        despesasFiltradas = this.recuperarTodosRegistros()
+
+        if(despesa.ano != ''){
+            despesasFiltradas = despesasFiltradas.filter(d => d.ano == despesa.ano)
+        }
+
+        if(despesa.mes != ''){
+            despesasFiltradas = despesasFiltradas.filter(d => d.mes == despesa.mes)
+        }
+
+        if(despesa.dia != ''){
+            despesasFiltradas = despesasFiltradas.filter(d => d.dia == despesa.dia)
+        }
+
+        if(despesa.tipo != ''){
+            despesasFiltradas = despesasFiltradas.filter(d => d.tipo == despesa.tipo)
+        }
+        
+        if(despesa.descricao != ''){descricao
+            despesasFiltradas = despesasFiltradas.filter(d => d.descricao == despesa.descricao)
+        }
+
+        if(despesa.valor != ''){
+            despesasFiltradas = despesasFiltradas.filter(d => d.valor == despesa.valor)
+        }
+
+        return despesasFiltradas
+    }
+    
+    remover(id){
+        localStorage.removeItem(id)
     }
 }
 
@@ -92,6 +128,13 @@ function cadastrarDespesa() {
     if(despesa.validarDados()){
         bd.gravar(despesa)
 
+        ano.value =''
+        mes.value =''
+        dia.value =''
+        tipo.value =''
+        descricao.value =''
+        valor.value =''
+
         document.getElementById('modal_titulo').innerHTML = 'Registro inserido com sucesso'
 		document.getElementById('modal_titulo_div').className = 'modal-header text-success'
 		document.getElementById('modal_conteudo').innerHTML = 'Despesa foi cadastrada com sucesso!'
@@ -112,15 +155,16 @@ function cadastrarDespesa() {
     }
 }
 
-function carregaListaDespesas(){
+function carregaListaDespesas(despesas = Array(), filtro = false){
 
-    let despesas = Array()
-
-    despesas = bd.recuperarTodosRegistros()
+    if(despesas.length == 0 && filtro == false){
+        despesas = bd.recuperarTodosRegistros()
+    }
 
     let listaDespesas = document.getElementById('listaDespesas')
-
+    listaDespesas.innerHTML = ''
     despesas.forEach(function(d){
+
         //criar linhas
         var linha = listaDespesas.insertRow();
 
@@ -145,5 +189,36 @@ function carregaListaDespesas(){
         linha.insertCell(2).innerHTML = d.descricao
         linha.insertCell(3).innerHTML = d.valor
 
+        //botão de exclusão
+
+        let btn = document.createElement('button')
+        btn.className = 'btn btn-danger'
+        btn.innerHTML = '<i class="fa fa-times"  ></i>'
+        btn.id = `id_despesa_${d.id}`
+        btn.onclick = function(){
+            let id = this.id.replace('id_despesa_' , '')
+
+            bd.remover(id)
+            window.location.reload()
+        }
+        linha.insertCell(4).append(btn)
+
     })
+}
+
+function pesquisarDespesa(){
+
+    let ano = document.getElementById('ano').value
+    let mes = document.getElementById('mes').value
+    let dia = document.getElementById('dia').value
+    let tipo = document.getElementById('tipo').value
+    let descricao = document.getElementById('descricao').value
+    let valor = document.getElementById('valor').value
+
+    let despesa = new Despesa(ano, mes, dia, tipo, descricao, valor)
+
+    let despesas = bd.pesquisar(despesa)
+
+    this.carregaListaDespesas(despesas, true)
+
 }
